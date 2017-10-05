14d0383c5a3dae5b35b6c1a7dd282a1f649e4e8a
Eric Mountain <eric.stephen.mountain@gmail.com>
2017-10-05 23:40:05 +0200

    Adds pam_environment for gpg_agent activation

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 cd dotfiles/home
-ln --backup -s $(pwd)/bashrc ~/.bashrc
-ln --backup -s $(pwd)/emacs ~/.emacs
+[[ -L ~/.bashrc ]] || ln --backup -s $(pwd)/bashrc ~/.bashrc
+[[ -L ~/.emacs ]] || ln --backup -s $(pwd)/emacs ~/.emacs
+[[ -L ~/.pam_environment ]] || ln --backup -s $(pwd)/pam_environment ~/.pam_environment
 cd -
