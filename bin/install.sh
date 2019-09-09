325f11fefa171211a38a281dbe83e2089154598e
Eric Mountain <eric.mountain@gmail.com>
2019-09-10 00:10:08 +0200

    Transition to zsh and friends

--- bin/install.sh
+++ bin/install.sh
@@ -4,6 +4,16 @@ set -euo pipefail
 
 cd dotfiles/home
 [[ -L ~/.bashrc ]] || ln --backup -s $(pwd)/bashrc ~/.bashrc
+[[ -L ~/.zshrc ]] || ln --backup -s $(pwd)/zshrc ~/.zshrc
 [[ -L ~/.emacs ]] || ln --backup -s $(pwd)/emacs ~/.emacs
 [[ -L ~/.pam_environment ]] || ln --backup -s $(pwd)/pam_environment ~/.pam_environment
 cd -
+
+# These should have code reviews before installing really...
+if [ ! -d ~/.oh-my-zsh ] ; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/themes/powerlevel9k ] ; then
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+fi
