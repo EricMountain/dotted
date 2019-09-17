73e69de50d6790b74a5ac93f224285b44c47d497
Eric Mountain <eric.mountain@gmail.com>
2019-09-18 00:13:26 +0200

    Powerlevel 10k...

--- bin/install.sh
+++ bin/install.sh
@@ -7,13 +7,13 @@ if [ ! -d ~/.oh-my-zsh ] ; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-if [ ! -d ~/.oh-my-zsh/custom/themes/powerlevel9k ] ; then
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+if [ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k ] ; then
+    git clone --depth 1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 fi
 
 if grep -q 'NAME="Ubuntu"' /etc/os-release ; then
     if [ ! -d ~/.awesome-terminal-fonts ] ; then
-        git clone https://github.com/gabrielelana/awesome-terminal-fonts ~/.awesome-terminal-fonts
+        git clone --depth 1 https://github.com/gabrielelana/awesome-terminal-fonts ~/.awesome-terminal-fonts
         cd ~/.awesome-terminal-fonts
         ./install.sh
         cd - > /dev/null
