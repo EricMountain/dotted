df77e84bcedea405c9a7dee0d48e074385e4b27f
Eric Mountain <eric.mountain@gmail.com>
2022-08-16 22:50:01 +0200

    Latest java, python etc

--- bin/mac-setup/install.sh
+++ bin/mac-setup/install.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -euo pipefail
 
 # Stuff that maybe needs doing after running this:
 # * Fzf: To install useful keybindings and fuzzy completion: /usr/local/opt/fzf/install
@@ -7,16 +9,17 @@
 # * [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
 
 # https://github.com/derekparker/delve/blob/master/Documentation/installation/osx/install.md
-xcode-select --install
-#sudo /usr/sbin/DevToolsSecurity -enable
+
+# This has to be done by hand first. It opens a dialog.
+#xcode-select --install
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 for x in bash ack httpie git watch autojump ipcalc \
-    wget coreutils gnu-sed tmux fzf jq \
+    wget coreutils gnu-sed tmux fzf jq python virtualenv\
     ncdu parallel zsh findutils gcc make gpg2 pinentry bat gnu-tar \
     gnu-time hub grep git-delta tree yq \
-    direnv datamash zotero pyenv bluesnooze ; do
+    direnv datamash zotero pyenv bluesnooze java ; do
 	brew install $x
 done
 
@@ -25,7 +28,3 @@ for y in gifs iterm2 virtualbox gimp github \
          finicky ; do
     brew install --cask $y
 done
-
-pip3 install virtualenv
-
-brew cask install homebrew/cask-versions/adoptopenjdk8
