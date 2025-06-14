c2d6a9bcc0e50361da6aa6e7e9f73b05f013b470
Eric Mountain <eric.mountain@gmail.com>
2025-06-14 18:22:56 +0200

    fix cascadia fonts available in brew

--- bin/mac-setup/update.sh
+++ bin/mac-setup/update.sh
@@ -3,6 +3,6 @@
 brew update && brew upgrade && brew upgrade --cask
 
 # remove once done on all Macs
-brew install --cask font-cascadia-code font-cascadia-code-{nf,pl,mono,mono-nf,mono-pl}
+brew install --cask font-cascadia-code font-cascadia-code-{nf,pl}
 
 git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k pull
