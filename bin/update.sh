cac7dec08fdd82f38c9ca40f84f6ad360bf2456e
Eric Mountain <eric.mountain@gmail.com>
2025-06-14 20:15:11 +0200

    refactor update script

--- bin/update.sh
+++ bin/update.sh
@@ -13,6 +13,14 @@ done
 
 git -C "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" pull
 
+# MacOS ####################################################################
+if [ "$(uname)" = "Darwin" ]; then
+    brew update && brew upgrade && brew upgrade --cask
+
+    # TODO: remove once done on all Macs
+    brew install --cask font-caskaydia-cove-nerd-font
+fi
+
 # Local ####################################################################
 if [ -x ~/.local-config.d/update.sh ]; then
     ~/.local-config.d/update.sh
